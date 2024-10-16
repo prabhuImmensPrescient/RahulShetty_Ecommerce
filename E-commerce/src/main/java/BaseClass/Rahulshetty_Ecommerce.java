77fdd87c5f57aa627e23910877cd4b12ff3a6dc5
prabhuImmensPrescient <prabhu.k.singh@immenseprescient.com>
2024-10-16 15:16:49 +0530

    Login page

---
+++ E-commerce/src/main/java/BaseClass/Rahulshetty_Ecommerce.java
@@ -0,0 +1,32 @@
+package BaseClass;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Rahulshetty_Ecommerce {
+	
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+		driver.manage().deleteAllCookies();
+		
+		driver.get("https://rahulshettyacademy.com/client");
+		
+		WebElement username = driver.findElement(By.xpath("//input[@id='userEmail']"));
+		WebElement password = driver.findElement(By.xpath("//input[@id='userPassword']"));
+		WebElement loginbtn = driver.findElement(By.xpath("//input[@id='login']"));
+		
+		username.sendKeys("prabhusingh12296@gmail.com");
+		password.sendKeys("Prabhu123@");
+		loginbtn.click();
+		
+	Thread.sleep(5000);
+	driver.quit();
+	}
+}
